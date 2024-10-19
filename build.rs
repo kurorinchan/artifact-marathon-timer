7caa6058e6a322f47671c0ade9da2e853f47febc
kurorinchan <kurorinchan@users.noreply.github.com>
2024-10-19 11:25:47 +0900

    Run build.rs when src/protos/mod.rs does not exist
    Also in the workflow, build first before linting.

--- build.rs
+++ build.rs
@@ -1,9 +1,13 @@
 use std::path::Path;
 
 fn main() {
-    println!("cargo::rerun-if-changed=protos/storage.proto");
+    // Create the protos output directory first AND THEN tell cargo to rerun if
+    // the the protos are not there. Apparently, if the (generated) file
+    // (src/protos/mod.rs) does not exist, it runs this build script.
     let protos_output_dir = Path::new("src").join("protos");
     std::fs::create_dir_all(&protos_output_dir);
+    println!("cargo::rerun-if-changed=src/protos/mod.rs");
+    println!("cargo::rerun-if-changed=protos/storage.proto");
     protobuf_codegen::Codegen::new()
         .pure()
         .out_dir(&protos_output_dir)

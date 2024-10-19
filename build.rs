6b2a07033ea590eae52087302e60f005216cd1fd
kurorinchan <kurorinchan@users.noreply.github.com>
2024-10-19 10:51:20 +0900

    Create src/protos in build.rs
    It fails to generate protos if the directory does not exist.

--- build.rs
+++ build.rs
@@ -1,8 +1,12 @@
+use std::path::Path;
+
 fn main() {
     println!("cargo::rerun-if-changed=protos/storage.proto");
+    let protos_output_dir = Path::new("src").join("protos");
+    std::fs::create_dir_all(&protos_output_dir);
     protobuf_codegen::Codegen::new()
         .pure()
-        .out_dir("src/protos")
+        .out_dir(&protos_output_dir)
         .input("protos/storage.proto")
         .include("protos")
         .run()
